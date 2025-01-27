2cc2c99e288f62706f4c6365dcc84bc5c38f4540
fadizanbouah <fadi.zanbouah@gmail.com>
2025-01-27 02:24:28 +0200

    Revert "RoomSystemCheckpoint"
    This reverts commit dcb557d7212865134881b493f87ac96c1f915f8e.

--- RPSUnity/Assets/Scripts/RoomData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewRoom", menuName = "Game/RoomData")]
-public class RoomData : ScriptableObject
-{
-    public string roomName;
-    public GameObject roomPrefab;  // The prefab representing the room
-    public int weight = 1;         // Weight for random selection
-}
