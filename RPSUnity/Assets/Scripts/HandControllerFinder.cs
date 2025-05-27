7de22e1a4282013e3f43e5cc00e7bc66d5c8c764
fadizanbouah <fadi.zanbouah@gmail.com>
2025-05-27 18:56:55 +0300

    CHECKPOINT - Delete HandControllerFinder as it was redundant

--- RPSUnity/Assets/Scripts/HandControllerFinder.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public static class HandControllerFinder
-{
-    public static HandController Player =>
-        GameObject.FindWithTag("Player")?.GetComponent<HandController>();
-}
