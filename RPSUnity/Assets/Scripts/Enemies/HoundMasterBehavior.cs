40297ed80329f4f7f468cabb0444e56c6f3148c7
fadizanbouah <fadi.zanbouah@gmail.com>
2026-02-15 16:22:17 +0200

    CHECKPOINT - HooundMinionAttackAnimation WIP

--- RPSUnity/Assets/Scripts/Enemies/HoundMasterBehavior.cs
+++ RPSUnity/Assets/Scripts/Enemies/HoundMasterBehavior.cs
@@ -6,6 +6,11 @@ public class HoundMasterBehavior : MonoBehaviour, IEnemyBehavior
     private MinionController minionController;
     private HandController enemyHand;
 
+    private void Awake()
+    {
+        Debug.Log("[HoundMasterBehavior] Awake called");
+    }
+
     public void Initialize(HandController enemy, float[] configValues)
     {
         enemyHand = enemy;
@@ -36,50 +41,38 @@ public class HoundMasterBehavior : MonoBehaviour, IEnemyBehavior
             yield break;
         }
 
-        // NEW: Don't attack if enemy is already dead
+        // Don't attack if enemy is already dead
         if (enemyHand == null || enemyHand.health <= 0)
         {
             Debug.Log("[HoundMasterBehavior] Enemy is dead, hounds will not attack");
             yield break;
         }
 
-        // Only trigger if player didn't already lose this round
-        if (result == RoundResult.Win || result == RoundResult.Draw)
+        // Hounds attack regardless of round result (win/lose/draw)
+        var minions = minionController.GetActiveMinions();
+
+        Debug.Log($"[HoundMasterBehavior] {minions.Count} hounds will attempt to attack");
+
+        // Each hound tries to attack sequentially
+        foreach (var minionObj in minions)
         {
-            var minions = minionController.GetActiveMinions();
-
-            Debug.Log($"[HoundMasterBehavior] Player survived round, {minions.Count} hounds will attempt to attack");
-
-            // Each hound tries to attack sequentially
-            foreach (var minionObj in minions)
+            if (minionObj == null)
             {
-                if (minionObj == null)
-                {
-                    Debug.LogWarning("[HoundMasterBehavior] Minion object is null, skipping");
-                    continue;
-                }
+                Debug.LogWarning("[HoundMasterBehavior] Minion object is null, skipping");
+                continue;
+            }
 
-                HoundMinion hound = minionObj.GetComponent<HoundMinion>();
-                if (hound != null)
-                {
-                    Debug.Log($"[HoundMasterBehavior] Triggering attack for: {minionObj.name}");
-                    yield return hound.TryAttack(player);
-                }
-                else
-                {
-                    Debug.LogWarning($"[HoundMasterBehavior] No HoundMinion component found on {minionObj.name}");
-                }
+            HoundMinion hound = minionObj.GetComponent<HoundMinion>();
+            if (hound != null)
+            {
+                Debug.Log($"[HoundMasterBehavior] Triggering attack for: {minionObj.name}");
+                yield return hound.TryAttack(player);
+            }
+            else
+            {
+                Debug.LogWarning($"[HoundMasterBehavior] No HoundMinion component found on {minionObj.name}");
             }
         }
-        else
-        {
-            Debug.Log("[HoundMasterBehavior] Player lost round, hounds do not attack");
-        }
-    }
-
-    private void Awake()
-    {
-        Debug.Log("[HoundMasterBehavior] Awake called");
     }
 
     public IEnumerator OnBeforeRoundResolves(HandController player, string playerChoice, string enemyChoice)
